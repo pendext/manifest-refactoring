d0a7df832a0c15208d37bf34a7c3fb7613bc6079
John Pendexter <jpendexter@manifestcorp.com>
2014-02-02 14:50:59 -0500

    Moved the stringbuilder example to a new directory. This would be an example of refactoring, if you're reading these commit messages.

---
+++ src/main/java/com/manifestcorp/refactoring/stringbuilder/StringBuilder.java
@@ -0,0 +1,44 @@
+package com.manifestcorp.refactoring.stringbuilder;
+
+public class StringBuilder {
+
+    public String printOwingAndInvoiceOpeningStatement(double amount) {
+        String result = "";
+        result += "************ ************ ************\n";
+        result += "************ Your Invoice ************\n\n";
+        result += "************ ************\n";
+        result += "***   Amount Owed     ***\n";
+        result += "************ ************\n";
+        result += "\n\n\n";
+        result += "*** You owe $" + String.valueOf(amount) + " ***\n";
+        return result;
+    }
+
+    public String printRefund(double refundAmount) {
+        String result = "";
+        result += "************ ************\n";
+        result += "***   Amount Refunded ***\n";
+        result += "************ ************\n";
+        result += "\n\n\n";
+        result += "*** Your refund is $" + String.valueOf(refundAmount) + " ***\n";
+        return result;
+    }
+
+    public String printInvoiceClosingStatement(String customerName) {
+        String result = "";
+        result += "************ ************\n";
+        result += "************ ************\n";
+        result += "*** Thank you, " + customerName + "   ***\n";
+        result += "************ ************ ************\n";
+        result += "************ ************ ************\n";
+
+        return result;
+    }
+
+    public String printFullInvoice(double amountOwed, double refundAmount, String customerName) {
+        String result = printOwingAndInvoiceOpeningStatement(amountOwed);
+        result += printRefund(refundAmount);
+        result += printInvoiceClosingStatement(customerName);
+        return result;
+    }
+}
