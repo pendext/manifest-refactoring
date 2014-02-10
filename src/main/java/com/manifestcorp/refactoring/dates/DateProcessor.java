99bb307e21a577d02235de478faefe883168c706
John Pendexter <jpendexter@manifestcorp.com>
2014-02-09 20:49:52 -0500

    Minor refactoring for date processor class.

--- src/main/java/com/manifestcorp/refactoring/dates/DateProcessor.java
+++ src/main/java/com/manifestcorp/refactoring/dates/DateProcessor.java
@@ -7,26 +7,22 @@ import java.util.Date;
 public class DateProcessor {
 
     public void saveDueDate(Account account, String date) {
-        Date parsedDate = null;
-        try {
-            parsedDate = new SimpleDateFormat("yyyy-MM-dd").parse(date);
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-
+        Date parsedDate = parseDate(date);
         account.setDueDate(parsedDate);
     }
 
     public void saveStartDate(Account account, String date) {
+        Date parsedDate = parseDate(date);
+        account.setStartDate(parsedDate);
+    }
+
+    private Date parseDate(String date) {
         Date parsedDate = null;
         try {
             parsedDate = new SimpleDateFormat("yyyy-MM-dd").parse(date);
         } catch (ParseException e) {
             e.printStackTrace();
         }
-
-        account.setStartDate(parsedDate);
+        return parsedDate;
     }
-
-
 }
